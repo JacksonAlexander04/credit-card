1596772264a9db72b239b5ae4458098e7a8d0fe0
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2021-10-20 12:10:44 -0600

    Changes

--- Credit Card Algorithm/src/CreditCard.java
+++ Credit Card Algorithm/src/CreditCard.java
@@ -3,21 +3,34 @@ import java.util.Scanner;
 import java.io.IOException;
 
 public class CreditCard {
+	static int counter = 0;
+	static int[] individualNumbers = new int[16];
+	static Long[] creditCardNumbers = new Long[99];
 
 	public static void main(String[] args) throws IOException {
 		readTextFile();
+		checkValid();
 	}
-		public static void readTextFile() throws IOException {
-			Long [] cardNumber = new long[99];
-		Scanner myFile = new Scanner(new File("card.txt"));
-		int counter = 0;
-		while(myFile.hasNext()) {
-			String word = myFile.next();
-			System.out.println(word);
-			
+
+	public static void readTextFile() throws IOException {
+
+		Scanner myFile = new Scanner(new File("cardNumbers"));
+
+		while (myFile.hasNext()) {
+
+			Long Card = myFile.nextLong();
+			System.out.println(Card);
+			for (int i = 0; i < 99; i++) {
+				individualNumbers[i] = myFile.nextInt();
+			}
+
 		}
 
 	}
-	
+
+	public static void checkValid() throws IOException {
+		
+
+	}
 
 }

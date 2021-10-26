15cc5e70195b6bffe53d156b9e41a3af849ec81e
jackson.dalexander <jackson.dalexander@MU-C1-10.mullen.pvt.k12.co.us>
2021-10-26 11:10:22 -0600

    credit card

--- Credit Card Algorithm/src/CreditCard.java
+++ Credit Card Algorithm/src/CreditCard.java
@@ -3,32 +3,64 @@ import java.util.Scanner;
 import java.io.IOException;
 
 public class CreditCard {
+	//create array and variables
+	static long cardNumber;
 	static int counter = 0;
-	static int[] individualNumbers = new int[16];
-	static Long[] creditCardNumbers = new Long[99];
+	static long sum;
+	static Long[] numbers = new Long[16];
+	
+	static boolean validCard = true;
 
 	public static void main(String[] args) throws IOException {
-		readTextFile();
-		checkValid();
+		//get file
+		Scanner myFile = new Scanner(new File("newCreditCards"));
+		
+		while (myFile.hasNext()) {	
+				cardNumber = myFile.nextLong();
+				
+				checkValid();
+		}
+		//print out
+		System.out.println("You have " + counter + " valid credit cards.");
 	}
 
-	public static void readTextFile() throws IOException {
+	
+		
 
-		Scanner myFile = new Scanner(new File("cardNumbers"));
+	
 
-		while (myFile.hasNext()) {
-
-			Long Card = myFile.nextLong();
-			System.out.println(Card);
-			for (int i = 0; i < 99; i++) {
-				individualNumbers[i] = myFile.nextInt();
-			}
+	public static void checkValid() {
+		sum = 0;
+		long printNumber = cardNumber;
+		//get single digit
+		for(int a = 15; a >= 0; a --) {
+			
+			numbers[a] = cardNumber % 10;
+			cardNumber /= 10;
 
 		}
-
-	}
-
-	public static void checkValid() throws IOException {
+		//mulitiply every other
+		for(int b = 0; b < 16; b = b+2) {
+			numbers[b]= numbers[b] * 2;
+			
+			if(numbers[b] >= 10) {
+				long firstDigit = numbers[b] % 10;
+				long secondDigit = numbers[b] / 10;
+				numbers[b] = firstDigit + secondDigit;
+			}
+		}
+			for(int c = 0; c < numbers.length; c++) {
+				sum+=numbers[c];
+			}
+			if(sum % 10 == 0) {
+				counter++;
+				System.out.println(printNumber + "is valid");
+			}
+			else {
+				System.out.println(printNumber + "is unvalid");
+			}
+			
+			
 		
 
 	}
